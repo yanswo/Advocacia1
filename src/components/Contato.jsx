f67bb311c51dec14d359559d8b0d8e4d93b8592e
yanswo <yanlucas2202@gmail.com>
2025-01-11 19:23:24 -0300

    Contato e Localização concluídos, chances de dar upgrade em um breve.

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -5,12 +5,15 @@ const Contato = () => {
   const [formData, setFormData] = useState({
     nome: "",
     email: "",
-    mensagem: "",
+    numero: "",
+    causa: "",
+    comentario: "",
+    assunto: "",
+    mensagemAdicional: "",
     contato: "whatsapp",
   });
 
-  const handleOpenModal = (canal) => {
-    setFormData({ ...formData, contato: canal });
+  const handleOpenModal = () => {
     setIsModalOpen(true);
   };
 
@@ -27,37 +30,69 @@ const Contato = () => {
     e.preventDefault();
 
     if (formData.contato === "whatsapp") {
-      const whatsappUrl = `https://wa.me/123456789?text=Nome:%20${formData.nome}%0AEmail:%20${formData.email}%0AMensagem:%20${formData.mensagem}`;
+      if (!formData.numero) {
+        alert("Por favor, forneça seu número para contatar via WhatsApp.");
+        return;
+      }
+
+      const whatsappUrl = `https://wa.me/123456789?text=Nome:%20${formData.nome}%0A%0ANúmero:%20${formData.numero}%0ACausa:%20${formData.causa}%0AComentário:%20${formData.comentario}`;
       window.open(whatsappUrl, "_blank");
     } else if (formData.contato === "email") {
-      const mailtoLink = `mailto:contato@advocaciacristovao.com.br?subject=Contato%20via%20Formulário&body=Nome:%20${formData.nome}%0AEmail:%20${formData.email}%0AMensagem:%20${formData.mensagem}`;
+      if (!formData.email) {
+        alert("Por favor, forneça seu email para contatar via Email.");
+        return;
+      }
+
+      const mailtoLink = `mailto:contato@advocaciacristovao.com.br?subject=${formData.assunto}&body=Nome:%20${formData.nome}%0AEmail:%20${formData.email}%0ACausa:%20${formData.causa}%0AComentário:%20${formData.comentario}%0AMensagem%20Adicional:%20${formData.mensagemAdicional}`;
       window.location.href = mailtoLink;
     }
 
     setIsModalOpen(false);
   };
 
+  const handleSelectContact = (canal) => {
+    setFormData({ ...formData, contato: canal });
+  };
+
   return (
     <section id="contato">
       <h2>Contato</h2>
-      <p>Entre em contato através do WhatsApp ou Email!</p>
-      <div className="contato-links">
-        <a href="#" onClick={() => handleOpenModal("whatsapp")}>
-          WhatsApp
-        </a>
-        <a href="#" onClick={() => handleOpenModal("email")}>
-          Email
-        </a>
-      </div>
+      <p>Entre em contato para mais informações sobre nossos serviços.</p>
+      <br></br>
+      <button onClick={handleOpenModal} className="contact-button">
+        Entre em contato
+      </button>
 
       {isModalOpen && (
-        <div className="modal fadeIn">
+        <div className="modal-overlay fadeIn">
           <div className="modal-content">
             <span className="modal-close" onClick={handleCloseModal}>
               &times;
             </span>
-            <h3>Envie sua mensagem</h3>
-            <form onSubmit={handleSubmit}>
+            <h3>Escolha o meio de contato</h3>
+            <div className="contact-selection">
+              <button
+                onClick={() => handleSelectContact("whatsapp")}
+                className={`contact-option ${
+                  formData.contato === "whatsapp" ? "selected" : ""
+                }`}
+              >
+                WhatsApp
+              </button>
+              <button
+                onClick={() => handleSelectContact("email")}
+                className={`contact-option ${
+                  formData.contato === "email" ? "selected" : ""
+                }`}
+              >
+                Email
+              </button>
+            </div>
+
+            <form
+              onSubmit={handleSubmit}
+              className={`form-transition ${formData.contato}`}
+            >
               <div className="modal-form-group">
                 <label htmlFor="nome">Nome:</label>
                 <input
@@ -69,52 +104,83 @@ const Contato = () => {
                   required
                 />
               </div>
+
+              {formData.contato === "whatsapp" && (
+                <>
+                  <div className="modal-form-group">
+                    <label htmlFor="numero">Número:</label>
+                    <input
+                      type="tel"
+                      id="numero"
+                      name="numero"
+                      value={formData.numero}
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="modal-form-group">
+                    <label htmlFor="causa">Causa:</label>
+                    <input
+                      type="text"
+                      id="causa"
+                      name="causa"
+                      value={formData.causa}
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                </>
+              )}
+
+              {formData.contato === "email" && (
+                <>
+                  <div className="modal-form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="modal-form-group">
+                    <label htmlFor="assunto">Assunto:</label>
+                    <input
+                      type="text"
+                      id="assunto"
+                      name="assunto"
+                      value={formData.assunto}
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="modal-form-group">
+                    <label htmlFor="mensagemAdicional">
+                      Mensagem Adicional:
+                    </label>
+                    <textarea
+                      id="mensagemAdicional"
+                      name="mensagemAdicional"
+                      value={formData.mensagemAdicional}
+                      onChange={handleInputChange}
+                    ></textarea>
+                  </div>
+                </>
+              )}
+
               <div className="modal-form-group">
-                <label htmlFor="email">Email:</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-              <div className="modal-form-group">
-                <label htmlFor="mensagem">Mensagem:</label>
+                <label htmlFor="comentario">Comentário:</label>
                 <textarea
-                  id="mensagem"
-                  name="mensagem"
-                  value={formData.mensagem}
+                  id="comentario"
+                  name="comentario"
+                  value={formData.comentario}
                   onChange={handleInputChange}
                   required
                 ></textarea>
               </div>
-              <div className="modal-radio-group">
-                <label>Meio de Contato:</label>
-                <div>
-                  <input
-                    type="radio"
-                    id="whatsapp"
-                    name="contato"
-                    value="whatsapp"
-                    checked={formData.contato === "whatsapp"}
-                    onChange={handleInputChange}
-                  />
-                  <label htmlFor="whatsapp">WhatsApp</label>
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    id="email"
-                    name="contato"
-                    value="email"
-                    checked={formData.contato === "email"}
-                    onChange={handleInputChange}
-                  />
-                  <label htmlFor="email">Email</label>
-                </div>
-              </div>
+
               <button type="submit" className="submit-btn">
                 Enviar
               </button>
--- src/components/Localizacao.jsx
+++ src/components/Localizacao.jsx
@@ -1,8 +1,22 @@
 const LocalizacaoSection = () => {
   return (
-    <section id="localizacao">
+    <section id="localizacao" className="minimalista">
       <h2>Localização</h2>
       <p>Endereço: Rua Exemplo, 123, Cidade - Estado</p>
+
+      {/* Mapa Integrado - Google Maps */}
+      <div className="map-container">
+        <iframe
+          title="Localização do Escritório"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3933.315865293431!2d-35.7175215125494!3d-9.654028862440184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x70145a3242d3263%3A0xa6595dadf066018a!2sRua%20Hamilton%20de%20Barros%20Soutinho%2C%201899-1775%20-%20Jati%C3%BAca%2C%20Macei%C3%B3%20-%20AL%2C%2057035-690!5e0!3m2!1spt-BR!2sbr!4v1736633725278!5m2!1spt-BR!2sbr"
+          width="100%"
+          height="300"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          aria-hidden="false"
+          tabIndex="0"
+        ></iframe>
+      </div>
     </section>
   );
 };
