1612b3972cbf7ba537aed8e77a845633bbb1618a
Willian Oliveira <williancontato67@gmail.com>
2025-01-11 11:43:37 -0300

    Inicialização das fotos do escritorio

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,8 +1,34 @@
+import imgEscrit01 from '../imgs/escritorio01.jpg';
+import imgEscrit02 from '../imgs/escritorio01.jpg';
+import imgEscrit03 from '../imgs/escritorio02.jpg';
+import imgEscrit04 from '../imgs/escritorio04.jpg';
+import imgEscrit05 from '../imgs/escritorio06.jpg';
+import imgEscrit06 from '../imgs/escritorio04.jpg';
+
 const GaleriaSection = () => {
   return (
     <section id="galeria">
       <h2>Fotos do Escritório</h2>
-      <p>Imagens do escritório e eventos.</p>
+      <ul className="cardsConteiner">
+        <li className="card">
+          <img src={imgEscrit01} alt="imagem escritorio" />
+        </li>
+        <li className="card">
+          <img src={imgEscrit03} alt="imagem escritorio" />
+        </li>
+        <li className="card">
+          <img src={imgEscrit02} alt="imagem escritorio" />
+        </li>
+        <li className="card">
+          <img src={imgEscrit04} alt="imagem escritorio" />
+        </li>
+        <li className="card">
+          <img src={imgEscrit05} alt="imagem escritorio" />
+        </li>
+        <li className="card">
+          <img src={imgEscrit06} alt="imagem escritorio" />
+        </li>
+      </ul>
     </section>
   );
 };
