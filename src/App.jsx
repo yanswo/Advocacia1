1ed6667c63614a91f7edfa551c6a286ed20c1e07
Willian Oliveira <williancontato67@gmail.com>
2025-01-30 23:33:29 -0300

    Finalização das noticias

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Header from "./components/Header";
-import HeroSection from "./components/HeroSection";
-import Sobre from "./components/Sobre";
-import Artigos from "./components/Artigos";
-import Galeria from "./components/Galeria";
-import Localizacao from "./components/Localizacao";
-import Contato from "./components/Contato";
-import Footer from "./components/Footer";
-import Cards from "./components/Cards";
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import Sobre from './components/Sobre';
+import Artigos from './components/Artigos';
+import Galeria from './components/Galeria';
+import Localizacao from './components/Localizacao';
+import Contato from './components/Contato';
+import Footer from './components/Footer';
+import Cards from './components/Cards';
+import NoticiasConteiner from './components/noticias/NoticiasConteiner'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Sobre />
       <Cards />
       <Artigos />
+      <NoticiasConteiner />
       <Galeria />
       <Localizacao />
       <Contato />
---
+++ src/components/noticias/NoticiaModal.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import style from './NoticiaModal.module.css';
+
+function fecharModal(target, currentTarget, setActiveModal) {
+  if (target === currentTarget) setActiveModal(false);
+}
+
+const NoticiaModal = ({ setActiveModal, noticia }) => {
+  return (
+    <section
+      className={style.modalConteiner}
+      onClick={({ target, currentTarget }) =>
+        fecharModal(target, currentTarget, setActiveModal)
+      }
+    >
+      <div className={style.modalContent}>
+        <div className={style.content}>
+          <div className={style.bannerNoticia}>
+            <div style={{ backgroundImage: `url(${noticia.img})` }}></div>
+            <h1>{noticia.titulo}</h1>
+          </div>
+          <div className={style.contentNoticia}>
+            {noticia.noticias.map(({id, texto, img}) => {
+              return (
+                <div key={id} className={style.divNoticia}>
+                  <div style={{ backgroundImage: `url(${img})` }}></div>
+                  <p>{texto}</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NoticiaModal;
